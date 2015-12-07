4b3daff91331303b9e480f4ad0f6be84024258a3
Pedro Rodrigues <medecau@gmail.com>
2015-12-07 11:01:46 +0000

    day 2

---
+++ 2.py
@@ -0,0 +1,23 @@
+import itertools
+from utils import get_input
+
+dimensions_list = get_input('2.txt').split('\n')
+
+total_area = 0
+total_ribon = 0
+for dims in dimensions_list:
+    dims = sorted(map(int, dims.split('x')))
+
+    smallest_perimeter = (dims[0] + dims[1]) * 2
+    volume = reduce(lambda x, y: x * y, dims)
+    total_ribon += volume + smallest_perimeter
+
+    sides = list(itertools.combinations(dims, 2))
+
+    areas = map(lambda side: side[0] * side[1], sides)
+    areas += areas
+    areas.append(areas[0])
+
+    total_area += sum(areas)
+
+print total_area, total_ribon
