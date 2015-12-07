6b9720bf1978cd971651e259693732168dccca68
Pedro Rodrigues <medecau@gmail.com>
2015-12-07 21:19:49 +0000

    day 6 part 2

--- 6.py
+++ 6.py
@@ -3,8 +3,7 @@ import itertools
 
 
 instructions = get_input('6.txt').split('\n')
-
-matrix = [[False] * 1000] * 1000
+matrix = [[0] * 1000] * 1000
 matrix = map(list, matrix)
 for i in instructions:
     parts = i.split(' ')
@@ -14,11 +13,12 @@ for i in instructions:
     for x in range(origin[0], destiny[0] + 1):
         for y in range(origin[1], destiny[1] + 1):
             if parts[0] == 'toggle':
-                matrix[x][y] = not matrix[x][y]
+                matrix[x][y] += 2
             elif parts[0] == 'turn':
                 if parts[1] == 'on':
-                    matrix[x][y] = True
-                else:
-                    matrix[x][y] = False
+                    matrix[x][y] += 1
+                elif matrix[x][y] > 0:
+                    matrix[x][y] -= 1
 flat = list(itertools.chain(*matrix))
-print(len([h for h in flat if h is True]))
+
+print(sum(flat))
