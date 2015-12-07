64fb707f1fd626eb4802d3b06a684f0e6fcad1f7
Pedro Rodrigues <medecau@gmail.com>
2015-12-07 11:42:15 +0000

    day 4

---
+++ 4.py
@@ -0,0 +1,19 @@
+import hashlib
+
+
+def hex_hash(s):
+    h = hashlib.md5()
+    h.update(s)
+    return h.hexdigest()
+
+zeros = '0' * 6
+key = 'ckczppom'
+
+num = 0
+while True:
+    num += 1
+    s = key + str(num)
+    r = hex_hash(s)
+    if r.startswith(zeros):
+        break
+print num, s
