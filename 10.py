9f91eb43bd451d04f8b827757c4967f00a195889
Pedro Rodrigues <medecau@gmail.com>
2015-12-10 09:27:05 +0000

    day 10

---
+++ 10.py
@@ -0,0 +1,22 @@
+def desc(i):
+    p = None
+    for e in i:
+        if p is None:
+            p = e
+            cntr = 1
+        elif e != p:
+            yield str(cntr)
+            yield p
+            cntr = 1
+            p = e
+        else:
+            cntr += 1
+    yield str(cntr)
+    yield p
+
+s = '3113322113'
+for i in range(50):
+    s = desc(s)
+
+result = ''.join(str(c) for c in s)
+print len(result)
