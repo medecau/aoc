d108fc80de7488e2aacfd34b7c1622f2c826bd18
Pedro Rodrigues <medecau@gmail.com>
2015-12-13 09:27:19 +0000

    day 13 part 1

---
+++ 13.py
@@ -0,0 +1,43 @@
+import re
+from itertools import permutations
+from collections import defaultdict
+
+
+instructions = """Alice would gain 54 happiness units by sitting next to Bob.
+Alice would lose 79 happiness units by sitting next to Carol.
+Alice would lose 2 happiness units by sitting next to David.
+Bob would gain 83 happiness units by sitting next to Alice.
+Bob would lose 7 happiness units by sitting next to Carol.
+Bob would lose 63 happiness units by sitting next to David.
+Carol would lose 62 happiness units by sitting next to Alice.
+Carol would gain 60 happiness units by sitting next to Bob.
+Carol would gain 55 happiness units by sitting next to David.
+David would gain 46 happiness units by sitting next to Alice.
+David would lose 7 happiness units by sitting next to Bob.
+David would gain 41 happiness units by sitting next to Carol.""".splitlines()
+
+instructions = open('13.txt').readlines()
+
+pattern = re.compile(r'(?P<sub>.*) would (?P<type>lose|gain) (?P<qty>\d+) .* to (?P<obj>[^\.]+)')
+
+graph = defaultdict(defaultdict)
+for i in instructions:
+    match = pattern.match(i).groupdict()
+    qty = int(match['qty'])
+    if match['type'] == 'lose':
+        qty *= -1
+    graph[match['sub']][match['obj']] = qty
+
+roster = graph.keys()
+
+best = None
+for p in permutations(roster):
+    change = 0
+    for i in range(len(p)):
+        sub = p[i]
+        left = p[i - 1]
+        right = p[i + 1] if i + 1 < len(p) else p[0]
+        change += graph[sub][left] + graph[sub][right]
+    if best is None or best < change:
+        best = change
+print best
